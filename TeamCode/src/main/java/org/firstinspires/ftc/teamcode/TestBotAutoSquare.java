75174484f5c434afe17d0df42313415b0ab74341
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2018-07-24 21:57:55 -0500

    Added drive and turn methods to square autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBotAutoSquare.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBotAutoSquare.java
@@ -14,7 +14,43 @@ public class TestBotAutoSquare extends OpMode
     int state = 0;  // used to represent the current state in the state machine
     int initialPosition = 0;  // used to grab the position of a robot at the beginning of a move
 
-    /*
+//////////////////////////
+
+    void driveStraight(double power, double distance)
+    {
+        robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        initialPosition = robot.leftDrive.getCurrentPosition();
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(power);
+
+        if((robot.leftDrive.getCurrentPosition() - initialPosition) > distance )
+        {
+            robot.leftDrive.setPower(0.0);
+            robot.rightDrive.setPower(0.0);
+        }
+    }
+
+//////////////////////////
+
+    void turnCorner(double power, double distance)
+    {
+        robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        initialPosition = robot.leftDrive.getCurrentPosition();
+        robot.leftDrive.setPower(power);
+        robot.rightDrive.setPower(power);
+
+        if((robot.leftDrive.getCurrentPosition() - initialPosition) > distance)
+        {
+            robot.leftDrive.setPower(0.0);
+            robot.rightDrive.setPower(0.0);
+        }
+    }
+
+
+
+    /**
      * Code to run ONCE when the driver hits INIT
      */
     @Override
@@ -32,14 +68,14 @@ public class TestBotAutoSquare extends OpMode
         robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    /*
+    /**
      * Code to run ONCE when the driver hits PLAY
      */
     @Override
     public void start() {
     }
 
-    /*
+    /**
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
     @Override
@@ -49,142 +85,45 @@ public class TestBotAutoSquare extends OpMode
         {
             // Drive forward 500 ticks and then turn for 50 ticks
             case 0:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.5);
-                robot.rightDrive.setPower(0.5);
+                driveStraight(0.5, 500);
                 state++;
                 break;
             case 1:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 500 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
+                turnCorner(0.25, 50);
+                state++;
                 break;
+
+////////////////////////////////////
+            // Drive forward 500 ticks and then turn for 50 ticks
             case 2:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.25);
-                robot.rightDrive.setPower(0.25);
+                driveStraight(.5, 500);
                 state++;
                 break;
             case 3:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 50 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
+                turnCorner(0.25, 50);
                 break;
 
 /////////////////////////////////////
             // Drive forward for 500 ticks and then turn for 50 ticks
             case 4:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.5);
-                robot.rightDrive.setPower(0.5);
+                driveStraight(0.5, 500);
                 state++;
                 break;
             case 5:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 500 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
+                turnCorner(0.25, 50);
+                state++;
                 break;
+
+////////////////////////////////////
+            //Drive forward for 500 ticks and then turn for 50 ticks
             case 6:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.25);
-                robot.rightDrive.setPower(0.25);
+                driveStraight(0.5, 500);
                 state++;
                 break;
             case 7:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 50 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
-                break;
-
-/////////////////////////////////////
-            // Drive forward for 500 ticks and then turn for 50 ticks
-            case 8:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.5);
-                robot.rightDrive.setPower(0.5);
+                turnCorner(0.25, 50);
                 state++;
                 break;
-            case 9:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 500 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
-                break;
-            case 10:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.25);
-                robot.rightDrive.setPower(0.25);
-                state++;
-                break;
-            case 11:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 50 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
-                break;
-
-/////////////////////////////////////
-            //drive forward for 500 ticks and then turn for 50 ticks
-            case 12:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.5);
-                robot.rightDrive.setPower(0.5);
-                state++;
-                break;
-            case 13:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 500 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    state++;
-                }
-                break;
-            case 14:
-                robot.leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                robot.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-                initialPosition = robot.leftDrive.getCurrentPosition();
-                robot.leftDrive.setPower(0.25);
-                robot.rightDrive.setPower(0.25);
-                state++;
-                break;
-            case 15:
-                if ( (robot.leftDrive.getCurrentPosition() - initialPosition) > 50 )
-                {
-                    robot.leftDrive.setPower(0.0);
-                    robot.rightDrive.setPower(0.0);
-                    resetStartTime();
-                    state++;
-                }
 
 ////////////////////////////////
             // Wait 2 seconds and then spin around for 2 seconds
@@ -209,7 +148,7 @@ public class TestBotAutoSquare extends OpMode
         }
     }
 
-    /*
+    /**
      * Code to run ONCE after the driver hits STOP
      */
     @Override
