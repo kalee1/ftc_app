ab44389c1d698cee420f4af4f5554f625e37fde8
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2018-09-13 22:31:05 -0500

    New Class Structure - First Trimester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by andrew on 9/13/18.
+ */
+
+public class Bot
+{
+
+    Chassis theChassis = new Chassis();
+
+    public void init()
+    {
+
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by andrew on 9/13/18.
+ */
+
+public class Chassis
+{
+    public void init()
+    {
+
+    }
+}
