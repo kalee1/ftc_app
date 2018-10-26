9f67859fa9625c47feac44b69431c76527faf2d0
WINDOWS-GD7IFRK\Ben <btate4502@gmail.com>
2018-10-26 16:42:26 -0500

    added arm code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmTeleop.java
@@ -0,0 +1,103 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "ArmTeleop" , group = "REV" )
+public class ArmTeleop extends OpMode {
+
+    public Servo Swivel = null;
+    public Servo Elbow = null;
+    public Servo Shoulder = null;
+
+    double gamma;
+    double theta;
+    double alpha;
+
+    public ArmTeleop(){
+    }
+
+    @Override
+    public void init() {
+
+
+
+        telemetry.addData("msg1", "Init" );
+
+        gamma = .5;
+        Swivel = hardwareMap. servo .get( "Swivel" );
+        Swivel.setPosition(gamma);
+
+        alpha = .18;
+        Shoulder = hardwareMap. servo .get( "Shoulder" );
+        Shoulder.setPosition(alpha);
+
+        theta = .5;
+        Elbow = hardwareMap. servo .get( "Elbow" );
+        Elbow.setPosition(theta);
+
+    }
+    @Override
+    public void loop() {
+
+        telemetry.addData("msg1", "Loop" );
+
+        //Swivel
+        gamma = gamma + (gamepad2.left_stick_x / 1000.0);
+
+        telemetry.addData("msg1","" + gamma);
+
+        Swivel.setPosition(gamma);
+
+        //Shoulder
+        /*
+        alpha = alpha + (gamepad2.left_stick_y / 1000.0);
+
+        telemetry.addData("msg2","" + alpha);
+
+        Shoulder.setPosition(alpha);
+        */
+        //Elbow
+        theta = theta + (gamepad2.right_stick_y / 1000.0);
+
+        telemetry.addData("msg3","" + theta);
+
+        Elbow.setPosition(theta);
+
+
+    }
+    @Override
+    public void stop() {
+    }
+}
