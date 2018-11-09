93e7cf9c41121723e67f237a4451a9a2657afced
WINDOWS-GD7IFRK\Ben <btate4502@gmail.com>
2018-11-09 17:00:51 -0600

    adding mark deploy functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarkDeploy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarkDeploy.java
@@ -1,25 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class MarkDeploy
 {
+    public Servo Flag;
 
     public void init(HardwareMap hwMap)
     {
-
+        try {
+            Flag = hwMap.servo.get( "Elbow" );
+            Flag.setDirection(Servo.Direction.FORWARD);
+            Flag.setPosition(.5);
+        } catch (Exception p_exception) {
+            Flag = null;
+        }
     }
-
     public void deploy()
     {
-
+        Flag.setPosition(.4);
     }
-
     public void retract()
     {
-
+        Flag.setPosition(.3);
     }
-
-
 }
