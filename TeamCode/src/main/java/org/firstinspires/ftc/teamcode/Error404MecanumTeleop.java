6d72a91ed7d511b17a7b220152cdecb704573054
WINDOWS-GD7IFRK\Ben <btate4502@gmail.com>
2018-11-08 20:34:36 -0600

    Updating MecanumTeleop with the latest code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404MecanumTeleop.java
@@ -243,13 +243,13 @@ public class Error404MecanumTeleop extends OpMode
         {
             swivelTarTime = System.currentTimeMillis() + 30;//was 63
 
-            if (gamepad2.right_stick_x < 0)
+            if (gamepad2.dpad_right == true)
             {
                 gamma += increment;
                 Swivel.setPosition(gamma);
                 telemetry.addData("Swivel positive increment", ""+ gamepad2.right_stick_x);
             }
-            else if (gamepad2.right_stick_x > 0)
+            else if (gamepad2.dpad_left == true)
             {
                 gamma -= increment;
                 Swivel.setPosition(gamma);
