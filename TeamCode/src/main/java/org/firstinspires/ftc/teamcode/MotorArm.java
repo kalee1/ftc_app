937623cbdd4acac6fb09fd6c66f7e3a2872e1607
WINDOWS-GD7IFRK\Ben <btate4502@gmail.com>
2018-12-10 14:30:06 -0600

    Accidental comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorArm.java
@@ -23,33 +23,27 @@ public class MotorArm
     public void Arminit(HardwareMap hwmap, Telemetry telem)
     {
 
-        Elbow = hwmap.dcMotor.get("Elbow");
-        Shoulder = hwmap.dcMotor.get("Shoulder");
-        Shoulderfront = hwmap.touchSensor.get("Shoulderfront");
-
-        Elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        Elbow = hwmap.dcMotor.get("Elbow");
+//        Shoulder = hwmap.dcMotor.get("Shoulder");
+//        Shoulderfront = hwmap.touchSensor.get("Shoulderfront");
+//
+//        Elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        Shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void ArmDrive( double RightStickY, double LeftStickY, Telemetry telem)
     {
-
-        Elbow.setPower(RightStickY * .5);
-
-        Shoulder.setPower(LeftStickY * .6);
-
-        if (Shoulderfront.isPressed())
-        {
-            Shoulder.setPower(0.0);
-
-            Shoulder = null;
-        }
-
-
-
-
+//
+//        Elbow.setPower(RightStickY * .5);
+//
+//        Shoulder.setPower(LeftStickY * .6);
+//
+//        if (Shoulderfront.isPressed())
+//        {
+//            Shoulder.setPower(0.0);
+//
+//            Shoulder = null;
 
     }
-
 }
 
