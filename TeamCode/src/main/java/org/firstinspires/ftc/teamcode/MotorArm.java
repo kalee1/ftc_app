93161480c174e5d0da14f1a1f0ccf3628f46e236
WINDOWS-GD7IFRK\Ben <btate4502@gmail.com>
2018-12-10 19:22:02 -0600

    Deleting spacing and adding trys & catches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorArm.java
@@ -1,19 +1,16 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 
 public class MotorArm
 {
-
     DcMotor elbow = null;
     DcMotor shoulder = null;
-
     protected TouchSensor shoulderFront = null;
+    protected TouchSensor elbowFront = null;
+    protected TouchSensor elbowRear = null;
 
     // Constructor for Class
     public MotorArm()
@@ -22,29 +19,75 @@ public class MotorArm
 
     public void init(HardwareMap hwmap, Telemetry telem)
     {
-
-        elbow = hwmap.dcMotor.get("elbow");
-        shoulder = hwmap.dcMotor.get("shoulder");
-        shoulderFront = hwmap.touchSensor.get("shoulderFront");
-
-        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        try
+        {
+            elbow = hwmap.dcMotor.get("elbow");
+            elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        catch (Exception p_exeception)
+        {
+            elbow = null;
+        }
+        try
+        {
+            shoulder = hwmap.dcMotor.get("shoulder");
+            shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        catch (Exception p_exeception)
+        {
+            shoulder = null;
+            shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        try
+        {
+            shoulderFront = hwmap.touchSensor.get("shoulderFront");
+        }
+        catch (Exception p_exeception)
+        {
+            shoulderFront = null;
+        }
+        try
+        {
+            elbowFront = hwmap.touchSensor.get("elbowFront");
+        }
+        catch (Exception p_exeception)
+        {
+            elbowFront = null;
+        }
+        try
+        {
+            elbowRear = hwmap.touchSensor.get("elbowRear");
+        }
+        catch (Exception p_exeception)
+        {
+            elbowRear = null;
+        }
     }
 
     public void armDrive( double RightStickY, double LeftStickY, Telemetry telem)
     {
-
-        elbow.setPower(RightStickY * .5);
-
-        shoulder.setPower(LeftStickY * .6);
-
         if (shoulderFront.isPressed())
         {
             shoulder.setPower(-0.2);
-
-            shoulder = null;
+        }
+        else
+        {
+            elbow.setPower(RightStickY * .5);
+            shoulder.setPower(LeftStickY * .6);
         }
 
+        if (elbowFront.isPressed())
+        {
+            elbow.setPower(-0.2);
+        }
+        else if(elbowRear.isPressed())
+        {
+            elbow.setPower(0.2);
+        }
+        else
+        {
+            elbow.setPower(RightStickY * .5);
+            shoulder.setPower(LeftStickY * .6);
+        }
     }
 }
-
