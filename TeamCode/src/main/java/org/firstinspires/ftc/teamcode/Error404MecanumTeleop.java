e81841b8e6e16907e3da4d90fcbf2fd7aeaa426c
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2019-01-15 20:05:06 -0600

    Modifying the teleop to driver specifications

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404MecanumTeleop.java
@@ -92,12 +92,12 @@ public class Error404MecanumTeleop extends OpMode
 
         //mineral intake controls
         //If the left bumper is pressed, turn the intake wheels inward.
-        if (gamepad2.left_bumper)
+        if (gamepad2.right_bumper)
         {
             robot.intake();
         }
         //If the right bumber is pressed, turn the intake wheels outward.
-        else if (gamepad2.right_bumper)
+        else if (gamepad2.left_bumper)
         {
             robot.eject();
         }
@@ -112,14 +112,14 @@ public class Error404MecanumTeleop extends OpMode
         /*Allows the secondary gamepad (which run the mineral arm) to turn the chassis at a very slow
                speed to aid in lining up on minerals. */
         //If the right trigger on the secondary gamepad is pressed, rotate chassis right at .15 power.
-        if(gamepad2.right_trigger > .1)
+        if(gamepad1.right_trigger > .1)
         {
-            robot.joystickDrive(0.0, 0.0, 1, 0.0, .15);
+            robot.joystickDrive(0.0, 0.0, 1, 0.0, .18);
         }
         //If the left trigger on the secondary gamepad is pressed, rotate the chassis left at .15 power.
-        else if(gamepad2.left_trigger > .1)
+        else if(gamepad1.left_trigger > .1)
         {
-            robot.joystickDrive(0.0, 0.0, -1, 0.0, .15);
+            robot.joystickDrive(0.0, 0.0, -1, 0.0, .18);
         }
 
 
@@ -138,9 +138,13 @@ public class Error404MecanumTeleop extends OpMode
         double powerLimit;
 
         //If the left trigger on the primary gamepad is pressed, set the maximum drive power to 1.0
-        if (gamepad1.left_trigger == 1)
+        if (gamepad1.left_bumper)
         {
             powerLimit = .7;
+        }
+        else if (gamepad1.right_bumper)
+        {
+            powerLimit = .2;
         }
         //If the left trigger on the primary gamepad is not pressed, set the maximum drive power to 0.5
         else
