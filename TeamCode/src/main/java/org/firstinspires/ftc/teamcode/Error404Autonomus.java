a118e867709a912f83dfe45384e963e57c953bed
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2019-03-15 15:52:55 -0500

    Minor logic changes to the fail-safe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404Autonomus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Error404Autonomus.java
@@ -181,7 +181,7 @@ public class Error404Autonomus extends OpMode
     protected double centerMineral;
 
     protected double mineralTurnDistance;
-    protected int fail = 0;
+    protected int fail;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -202,6 +202,7 @@ public class Error404Autonomus extends OpMode
         depoTurnHeading = new double[] {depoTurnHeadingL, depoTurnHeadingR, depoTurnHeadingC};
         direction = new double[] {directionL,directionG};
         goldPosition = "NoPosition";
+        fail = 0;
 
         // Send telemetry message to signify robot waiting;
 //        telemetry.addData("Say", "Let's Rock and Roll");
@@ -259,7 +260,7 @@ public class Error404Autonomus extends OpMode
                 }
                 break;
 
-                //check the pitch of the robot to ensure robot is not stuck
+            //check the pitch of the robot to ensure robot is not stuck
             case 2:
                 //robot is not stuck - continue with program
                 if(robot.goodPitch())
@@ -270,25 +271,26 @@ public class Error404Autonomus extends OpMode
                 //robot is stuck - execute fail-safe
                 else
                 {
+                    resetStartTime();
+                    robot.stopMotors();
+
                     //if robot has executed the fail-safe 3 times and is still stuck, stop executing
                     if(fail >= 3)
                     {
-                        resetStartTime();
-                        robot.stopMotors();
+
                         state = 999;
                     }
                     //stop the motors and enter fail-safe
                     else
                     {
-                        resetStartTime();
-                        robot.stopMotors();
+
                         state = 66;
                     }
                 }
                 break;
 
-                //fail-safe step 1:
-                //   drive forward to original landing position
+            //fail-safe step 1:
+            //   drive forward to original landing position
             case 66:
                 if(robot.drive(.2, forward, gain, 2, 4))
                 {
@@ -297,16 +299,15 @@ public class Error404Autonomus extends OpMode
                 }
                 break;
 
-                //fail-safe step 2:
-                //   extend hook for one second or 1000 encoders -- will extend the hanger all the
-                //   way out until it stalls up against the top of the hanger case -- then reset to
-                //   case 1 and reattempt detaching from lander
+            //fail-safe step 2:
+            //   extend hook for one second or 350 encoders -- will extend the hanger .25 inch --
+            //   then reset to case 0 and reattempt detaching from lander
             case 67:
-                if(robot.hangDrive(.9, 1000, Gen2_Hang.HangDirection.OUT) || getRuntime() > 1)
+                if(robot.hangDrive(.9, 350, Gen2_Hang.HangDirection.OUT) || getRuntime() > 1.25)
                 {
                     resetStartTime();
                     fail++;
-                    state = 1;
+                    state = 0;
                 }
                 break;
 
