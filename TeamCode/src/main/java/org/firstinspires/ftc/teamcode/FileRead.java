eab32db2431b07bf4114e6a5a3ee01e7ad63c7b9
Kristopher A. Lee <kristopher.a.lee@nasa.gov>
2019-04-02 22:20:57 -0500

    Added example code for building specific RobotAction classes from the CSV file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileRead.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileRead.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 @TeleOp(name="file read test", group="alpha")
 /**
@@ -29,19 +30,38 @@ public class FileRead extends OpMode
     {
         try
         {
+            RobotAction myAction;
             File theFile = new File("/storage/9016-4EF8/auto.csv");
             FileReader inputStreamReader = new FileReader(theFile);
 
             br = new BufferedReader(inputStreamReader);
-            telemetry.addData("bf: ", br);
             while((line = br.readLine()) != null)
             {
+                // Get a line from the CSV file and split it on the commas
                 String[] theItems = line.split(",");
-                for (String theData : theItems )
+
+                // Grab the first one... this will be the type of RobotAction to create
+                String type = theItems[0].trim().toUpperCase();
+
+                // Grab the reset of the items after the first one... these are the parameters to use
+                // with the constructors for each of the RobotAction subclasses.
+                String[] params = (Arrays.copyOfRange(theItems, 1, theItems.length));
+
+                // Based on the type, make the specific kind of RobotAction
+                if (type.equalsIgnoreCase("WAITACTION"))
                 {
-                    telemetry.addData("DataItem: ", theData);
+                    myAction = new WaitAction(params);
                 }
-            }
+                else if (type.equalsIgnoreCase("DRIVEACTION"))
+                {
+                    myAction = new DriveAction(params);
+                }
+                else
+                {
+                    myAction = new RobotAction();
+                }
+                telemetry.addData("TheAction: ", myAction);
+             }
 
         }
         catch(FileNotFoundException e)
